717dee977f6b7aeb44e6414ce07941720a1b6cdd
Javier de la Puente <javier.delapuente@canonical.com>
2024-08-13 17:12:47 +0200

    Fix

--- examples/django/django_app/testing/apps.py
+++ examples/django/django_app/testing/apps.py
@@ -1,9 +1,9 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from django.apps import WorkloadConfig
+from django.apps import AppConfig
 
 
-class TestingConfig(WorkloadConfig):
+class TestingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "testing"
